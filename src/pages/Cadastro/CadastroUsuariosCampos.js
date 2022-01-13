752fff89ce7a971acc9f5a42e8f4e4e380241d90
Hiran Ferreti Baccos <hiranfb@hotmail.com>
2022-01-13 09:26:25 -0300

    Commit Components Removidos

--- src/pages/Cadastro/CadastroUsuariosCampos.js
+++ src/pages/Cadastro/CadastroUsuariosCampos.js
@@ -1,28 +1,11 @@
 import React from 'react'
-import Background from '../../components/Background'
-import Input from '../../components/Input'
-import InputEmpty from '../../components/InputEmpty'
-import InputShort from '../../components/InputShort'
+
 
 function CadastroUsuariosCampos() {
     return (
         <div Style='display:flex; justify-content:center; align-items: center'>
             <div className='tituloInterno'>
                 <h3 Style='color:#555; margin-left: 400px'>Cadastro de usuários</h3>
-                <Background />
-                <div Style='display: flex; justify-content:space-evenly'>
-                    <Input />
-                    <Input />
-                    <Input />
-                </div>
-                <div Style='display: flex; justify-content:space-evenly'>
-                    <Input />
-                    <Input />
-                </div>
-                <div Style='display: flex; justify-content:space-evenly'>
-                    <InputEmpty />
-                    <InputShort />
-                </div>
             </div>
         </div>
     )
