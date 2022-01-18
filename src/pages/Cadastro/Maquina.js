3e64f6e6fe68ebe6bc1ac1d9ba8753fa1860a8c7
Hiran Ferreti Baccos <hiranfb@hotmail.com>
2022-01-18 13:13:04 -0300

    Commit Maquina

--- src/pages/Cadastro/Maquina.js
+++ src/pages/Cadastro/Maquina.js
@@ -4,78 +4,83 @@ import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import { IconContext } from "react-icons/lib";
 import { Button } from "react-bootstrap";
 import { GrEdit } from "react-icons/gr";
-import axios from 'axios';
+import Api from "../../services/Api";
+export default class Processo extends React.Component {
+  state = {
+    maquinas: [],
+  };
 
-export default function Maquina() {
+  componentDidMount() {
+    Api.get('/Maquina').then((res) => {
+      const maquinas = res.data;
+      this.setState({ maquinas });
+    });
+  }
 
-  const [maquina, setMaquina] = useState([])
+  render() {
 
-  const url = 'http://192.168.11.58:90/api/Maquina'
-
-  useEffect(() => {
-    axios.get(url)
-      .then((response) => setMaquina(response.data))
-      .catch((error) => {
-        console.log(error)
-      })
-  }, [])
-
-  return (
-    <>
-      <IconContext.Provider value={{ color: "#3cde3c", size: "1.6rem" }}>
-        <div className="container paddingContainer">
-          <div className="row">
-            <div className="col-md-6 col-sm-12">
-              <div className="tituloInterno">
-                <h2 Style="color:#555;">Maquina</h2>
+    return (
+      <>
+        <IconContext.Provider value={{ color: "#3cde3c", size: "1.6rem" }}>
+          <div className="container paddingContainer">
+            <div className="row">
+              <div className="col-md-6 col-sm-12">
+                <div className="tituloInterno">
+                  <h2 Style="color:#555;">Maquina</h2>
+                </div>
+              </div>
+              <div className="col-md-6 col-sm-12">
+                <div className="alignButtons">
+                  <Button variant="success">Cadastrar</Button>
+                </div>
               </div>
             </div>
-            <div className="col-md-6 col-sm-12">
-              <div className="alignButtons">
-                <Button variant="success">Cadastrar</Button>
+
+            <div className="row">
+              <div className="col-md-12 col-sm-12 paddingTop20Mobile">
+                <div Style="text-align: center" className="textTable">
+                  <table class="table table-striped">
+                    <thead>
+                      <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Processo</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Ordenação</th>
+                        <th scope="col">Tempo Médio Produção</th>
+                        <th scope="col">Opções/Editar</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {this.state.maquinas.map((maquina) => (
+                        <tr>
+                          <td Style='display: none'>{maquina.maquinaId}</td>
+                          <td>{maquina.nome}</td>
+                          <td>{maquina.processoId}</td>
+                          <td>{maquina.status ? 'Ativo' : 'Inativo'}</td>
+                          <td>{maquina.ordenacao}</td>
+                          <td>{maquina.tempoMedioProducao}</td>
+                          <td><span><GrEdit /></span></td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+
+            <div className="row paddingTop30">
+              <div className="col-md-6">
+                <Button variant="secondary">Voltar</Button>
+              </div>
+              <div className="col-md-6 paddingTop20Mobile">
+                <div className="alignButtons">
+                  <Button variant="success">Salvar</Button>
+                </div>
               </div>
             </div>
           </div>
-
-          <div className="row">
-            <div className="col-md-12 col-sm-12 paddingTop20Mobile">
-              <div Style="text-align: center" className="textTable">
-                <table class="table table-striped">
-                  <thead>
-                    <tr>
-                      <th scope="col">Nome</th>
-                      <th scope="col">Ordenação</th>
-                      <th scope="col">Opções</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td Style='display: none'>{maquina?.maquinaId}</td>
-                      <td>{maquina?.nome}</td>
-                      <td>{maquina?.processo}</td>
-                      <td>{maquina?.status}</td>
-                      <td>{maquina?.ordenacao}</td>
-                      <td>{maquina?.tempoMedioProducao}</td>
-                      <td><span><GrEdit /></span></td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-
-          <div className="row paddingTop30">
-            <div className="col-md-6">
-              <Button variant="secondary">Voltar</Button>
-            </div>
-            <div className="col-md-6 paddingTop20Mobile">
-              <div className="alignButtons">
-                <Button variant="success">Salvar</Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </IconContext.Provider>
-    </>
-  );
+        </IconContext.Provider>
+      </>
+    );
+  }
 }
