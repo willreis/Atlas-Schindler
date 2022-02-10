717b81ee00c84930126025bc6ea4648a4c243d27
willreis <william.reis.silva@gmail.com>
2022-02-10 14:39:35 -0300

    1ª parte modal Grupo de Acesso

--- src/pages/Cadastro/GruposAcesso.js
+++ src/pages/Cadastro/GruposAcesso.js
@@ -1,13 +1,111 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Modal from "react-bootstrap/Modal";
 import { IconContext } from "react-icons/lib";
-// import BootstrapTable from "react-bootstrap-table-next";
-// import filterFactory, { textFilter } from "react-bootstrap-table2-filter";
+import { VscEdit } from "react-icons/vsc";
+import { RiDeleteBinFill } from "react-icons/ri";
+import { Button } from "react-bootstrap";
+import { TiArrowForward } from "react-icons/ti";
+import { TiArrowBack } from "react-icons/ti";
+import "../../../src/grupo.css";
 
-// import Api from "../../services/Api";
+import BootstrapTable from "react-bootstrap-table-next";
+
+import Api from "../../services/Api";
 
 export default function GruposAcesso() {
- 
+  const columns = [
+    {
+      text: "Telas Sem Permissão",
+      dataField: "telas",
+    },
+  ];
+  const products = [
+    {
+      telas: "Usuario",
+    },
+    {
+      telas: "Usuario",
+    },
+    {
+      telas: "Usuario",
+    },
+    {
+      telas: "Usuario",
+    },
+  ];
+
+  var url = "GrupoDeAcesso";
+
+  //Modal const
+  const [show, setShow] = useState(false);
+
+  //GET
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    Api.get(`${url}`)
+      .then((response) => {
+        console.log(response);
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.log("Ops! Ocorreu um erro:", error);
+        alert("Ops! Ocorreu um erro:", error);
+      });
+  }, []);
+
+  // POST
+  const [nomeDoGrupo, setNomeDoGrupo] = useState();
+  const [descricaoDoGrupo, setDescricaoDoGrupo] = useState();
+  const [usuarios, setUsuarios] = useState(null);
+  const [telas, setTelas] = useState(null);
+  const [quantidadeTelasPermitidas, setQuantidadeTelasPermitidas] = useState();
+  const [quantidadeDeUsuarios, setQuantidadeDeUsuarios] = useState();
+
+  function handleRegister(e) {
+    // e.preventDefault();
+    handleRegister(user);
+  }
+
+  function createPost() {
+    Api.post(`/${url}`, {
+      nomeDoGrupo,
+      descricaoDoGrupo,
+      usuarios,
+      telas,
+      quantidadeTelasPermitidas,
+      quantidadeDeUsuarios,
+    })
+      .then((response) => {
+        // setNomeDoGrupo(response.data);
+        // setDescricaoDoGrupo(response.data);
+        // setUsuarios(response.data);
+        // setTelas(response.data);
+        // setQuantidadeTelasPermitidas(response.data);
+        // setQuantidadeDeUsuarios(response.data);
+        console.log(response.data);
+        alert("Cadastro efetuado com sucesso!");
+      })
+      .catch((error) => {
+        console.log("Ops! Ocorreu um erro!!!:", error);
+        alert("Ops! Ocorreu um erro!!!:", error);
+      });
+  }
+
+  //Delete
+  async function handleDeleteMaquina(grupoDeAcessoId) {
+    try {
+      await Api.delete(`/${url}/${grupoDeAcessoId}`);
+      setUser(
+        user.filter((grupo) => grupo.grupoDeAcessoId !== grupoDeAcessoId)
+      );
+      alert("Deletado com sucesso");
+    } catch (err) {
+      alert("erro ao deletar caso, tente novamente");
+    }
+  }
+
   return (
     <>
       <IconContext.Provider value={{ color: "#000000", size: "1.6rem" }}>
@@ -18,13 +116,150 @@ export default function GruposAcesso() {
                 <h2 Style="color:#555;">Grupo de Acesso</h2>
               </div>
             </div>
+            <div className="col-md-6">
+              <div Style="text-align: right">
+                <Button variant="success" onClick={() => setShow(true)}>
+                  Cadastrar
+                </Button>
+              </div>
+            </div>
           </div>
           <div className="row">
             <div className="col-md-12 col-sm-12 paddingTop20Mobile">
-              
+              <div className="textTable">
+                <table class="table table-striped table-bordered">
+                  <thead>
+                    <tr className="text-center">
+                      <th scope="col">Nome</th>
+                      <th scope="col">Descrição</th>
+                      <th scope="col">Qtd Telas Permitidas</th>
+                      <th scope="col">Qtd De Usuarios</th>
+                      <th scope="col">Editar / Excluir</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {user.map((grupo, index) => (
+                      <tr>
+                        <td Style="display:none" key={index}></td>
+                        <td Style="display:none">{grupo.grupoDeAcessoId}</td>
+                        <td>{grupo.nomeDoGrupo}</td>
+                        <td>{grupo.descricaoDoGrupo}</td>
+                        <td>{grupo.quantidadeTelasPermitidas}</td>
+                        <td>{grupo.quantidadeDeUsuarios}</td>
+                        <td className="text-center icons-table">
+                          <span
+                            Style="cursor:pointer"
+                            // onClick={() => pegarId(maquina.maquinaId)}
+                            alt="Editar"
+                          >
+                            <VscEdit />
+                          </span>
+
+                          <span
+                            Style="cursor:pointer"
+                            onClick={() =>
+                              handleDeleteMaquina(grupo.grupoDeAcessoId)
+                            }
+                            alt="Deletar"
+                          >
+                            <RiDeleteBinFill />
+                          </span>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
+
+        <div className="cadastroGPAcesso">
+          <Modal
+            size="lg"
+            show={show}
+            onHide={() => setShow(false)}
+            dialogClassName="modal-90w cadastroGPAcesso"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Cadastro de Grupo de Acesso
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div
+                className="formCadastro"
+                id="formCadastro"
+                Style="margin-bottom: 30px"
+              >
+                <div className="row">
+                  <form className="row formPadrao">
+                    <div className="row">
+                      <div className="col-5">
+                        <label>Nome</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Nome"
+                        />
+                      </div>
+                      <div className="col-5">
+                        <label>Descrição</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Descrição"
+                        />
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="col-md-2">
+                        <Button variant="success" Style="width:100%">
+                          Telas
+                        </Button>
+                      </div>
+                      <div className="col-md-2">
+                        <Button variant="success" Style="width:100%">
+                          Usuarios
+                        </Button>
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="col-md-4">
+                        <BootstrapTable
+                          keyField="id"
+                          data={products}
+                          columns={columns}
+                          bordered={false}
+                        />
+                      </div>
+                      <div className="col-md-2 center">
+                        <div>
+                          <TiArrowForward />
+                        </div>
+                        <div>
+                          <TiArrowBack />
+                        </div>
+                      </div>
+                      <div className="col-md-4">
+                        <BootstrapTable
+                          keyField="id"
+                          data={products}
+                          columns={columns}
+                          bordered={false}
+                          centered
+                        />
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </Modal.Body>
+          </Modal>
+        </div>
+        {/* Modal */}
       </IconContext.Provider>
     </>
   );
