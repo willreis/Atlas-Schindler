f1bb1e21346bc065049d66bd69054c44625fd25d
willreis <william.reis.silva@gmail.com>
2022-03-28 18:00:43 -0300

    dashboard

--- src/pages/Dashboard/AcompanhamentoEstoque.js
+++ src/pages/Dashboard/AcompanhamentoEstoque.js
@@ -6,6 +6,61 @@ import { ImArrowUp } from "react-icons/im";
 import { FaMinus } from "react-icons/fa";
 
 export default function AcompanhamentoEstoque() {
+  const produtos = [
+    {
+      id: 1,
+      cod: 26713,
+      qtd: 30,
+      semMovi: 25,
+      minEstoque: 30,
+      maxEstoque: 80,
+      estoqueAtual: 70,
+      statusAtual: "danger",
+    },
+    {
+      id: 2,
+      cod: 45682,
+      qtd: 50,
+      semMovi: 12,
+      minEstoque: 10,
+      maxEstoque: 580,
+      estoqueAtual: 370,
+      statusAtual: "warning",
+    },
+    {
+      id: 3,
+      cod: 56785,
+      qtd: 450,
+      semMovi: 50,
+      minEstoque: 30,
+      maxEstoque: 560,
+      estoqueAtual: 300,
+      statusAtual: "ok",
+    },
+    {
+      id: 4,
+      cod: 6543,
+      qtd: 34,
+      semMovi: 2,
+      minEstoque: 435,
+      maxEstoque: 3333,
+      estoqueAtual: 90,
+      statusAtual: "danger",
+    },
+    {
+      id: 5,
+      cod: 6543,
+      qtd: 3544,
+      semMovi: 222,
+      minEstoque: 454335,
+      maxEstoque: 333223,
+      estoqueAtual: 90,
+      statusAtual: "warning",
+    },
+  ];
+
+  // document.querySelector('#boxAcompEstoque').classList.add(produtos.map((e) =>(e.statusAtual)));
+
   return (
     <IconContext.Provider value={{ color: "#000000", size: "1.6rem" }}>
       <div className="paddingContainer">
@@ -16,57 +71,21 @@ export default function AcompanhamentoEstoque() {
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-12 acompanhamentoEstoqueRow bgVermelho">
-            <div>COD: 26713</div>
-            <div>Quantidade: 30 pçs</div>
-            <div>Sem Movimentação: 25 dias</div>
-            <div>
-              <ImArrowDown />
+        {produtos.map((prod) => (
+          <div className="row" key={prod.id}>
+            <div
+              id="boxAcompEstoque"
+              className={`col-md-12 acompanhamentoEstoqueRow ${prod.statusAtual}`}
+            >
+              <div>COD: {prod.cod}</div>
+              <div>Quantidade: {prod.qtd} pçs</div>
+              <div>Sem Movimentação: {prod.semMovi} dias</div>
+              <div>
+                <ImArrowDown />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12 acompanhamentoEstoqueRow bgVermelho">
-            <div>COD: 26790</div>
-            <div>Quantidade: 40 pçs</div>
-            <div>Sem Movimentação: 12 dias</div>
-            <div>
-              <ImArrowDown />
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12 acompanhamentoEstoqueRow bgAmarelo">
-            <div>COD: 26850</div>
-            <div>Quantidade: 60 pçs</div>
-            <div>Sem Movimentação: 15 dias</div>
-            <div>
-              <FaMinus />
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12 acompanhamentoEstoqueRow bgAmarelo">
-            <div>Id: 45</div>
-            <div>COD: 27390</div>
-            <div>Quantidade: 80 pçs</div>
-            <div>Sem Movimentação: 9 dias</div>
-            <div>
-              <FaMinus />
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12 acompanhamentoEstoqueRow bgAzul">
-            <div>COD: 32658</div>
-            <div>Quantidade: 120 pçs</div>
-            <div>Sem Movimentação: 30 dias</div>
-            <div>
-              <ImArrowUp />
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </IconContext.Provider>
   );
